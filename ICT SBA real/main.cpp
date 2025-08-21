8ba32848d50adf67da0600a0989657e0e02a3bc0
Kenny Lam <kennylamqwq@gmail.com>
2025-08-21 16:51:44 +0800

    Initial Commit

---
+++ ICT SBA real/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ICT SBA real
+//
+//  Created by Kenny Lam on 21/8/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
